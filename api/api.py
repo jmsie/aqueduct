dc421b32101b269a1e9513f035d5cef96522f2ce
jeff lu <jeff.lu0419@gmail.com>
2019-11-21 23:45:30 -0500

    Change set config to init

--- api/api.py
+++ api/api.py
@@ -1,9 +1,10 @@
 class API():
-  def __init__(self):
+  def __init__(self, api_config):
     self.name = ""
     self.base_url = ""
     self.api_key = ""
     self.api_config = None
+    self.set_api_config(api_config)
 
   def set_api_config(self, api_config):
     self.api_config = api_config
@@ -14,12 +15,17 @@ class API():
     except:
       print("Error in setting up API: " + self.name)
 
+  def set_positions(self):
+    pass
+
+  def get_positions(self):
+    pass
+
 if __name__ == "__main__":
   api_config = {
     'name': "C2",
     "base_url": "https://collective2.com/world/apiv3/",
     "api_key": "1kuBwZmJpHHHgJAHFSYGYFSYGHf1nvOV4hSGPWVwy8HuAshe",
   }
-  api = API()
-  api.set_api_config(api_config)
+  api = API(api_config)
 

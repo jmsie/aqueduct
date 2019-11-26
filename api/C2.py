8909c8825afdc77407d7d99117cf69702fe6a85d
jeff lu <jeff.lu0419@gmail.com>
2019-11-26 01:50:31 -0500

    Set/reset/commit position

--- api/C2.py
+++ api/C2.py
@@ -1,12 +1,32 @@
 import sys
-sys.path.append('..')
+import json
 
+sys.path.append('..')
 from api.API import API
 
 class C2(API):
   def __init__(self, context):
     super().__init__(context)
+    self.positions = []
 
+  def reset(self):
+    self.positions = []
+
+  def set_positions(self, symbol, symbol_type, position):
+    self.positions.append({
+      "symbol": symbol,
+      "typeofsymbol": symbol_type,
+      "quant": position,
+    })
+
+  def commit_positions(self, system_id, api_key):
+    data = {
+      "systemid": system_id,
+      "apikey": api_key,
+      "positions": self.positions,
+    }
+    payload = json.dumps(data)
+    print(payload)
 
 
 if __name__ == "__main__":

ffb234e5fa2a9da3672b30c10bacd2b0a2b92e48
jeff lu <jeff.lu0419@gmail.com>
2019-11-26 00:09:08 -0500

    Inject the API into monitor

--- monitor.py
+++ monitor.py
@@ -2,13 +2,16 @@ from os import walk
 from os.path import join
 
 
+
 class Monitor():
   def __init__(self):
     self.fund_settings = None
     self.funds = []
+    self.APIs = []
 
   def set_fund_settings(self, fund_settings):
     self.fund_settings = fund_settings
+    self.parse_fund_settings()
 
   def parse_fund_settings(self):
     def get_files_in_path(path):
@@ -26,6 +29,8 @@ class Monitor():
       }
       self.funds.append(fund)
 
+  def append_api(self, api):
+    self.APIs.append(api)
 
 
   def start(self):
@@ -35,16 +40,24 @@ class Monitor():
   def track_files(self):
     for fund in self.funds:
       total_position = 0
-      for path in fund['paths']:
+      fund_name = fund['name']
+      fund_paths = fund['paths']
+      for path in fund_paths:
         with open(path) as fd:
           # Only one line in the signal output file
           line = fd.readline()
           position = int(line.split(',')[1])
           total_position += position
+      for api in self.APIs:
+        system_id = self.fund_settings[fund_name]['system_id']
+        symbol = self.fund_settings[fund_name]['symbol']
+        api.set_positions(system_id, symbol, total_position)
 
 if __name__ == "__main__":
   from config import *
+  from api import C2
+  c2 = C2.C2(c2_config)
   monitor = Monitor()
+  monitor.append_api(c2)
   monitor.set_fund_settings(fund_settings)
-  monitor.parse_fund_settings()
   monitor.start()

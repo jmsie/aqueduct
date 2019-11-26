0e050b59eb037d1f8f63cec8d7f616e1a59753a6
jeff lu <jeff.lu0419@gmail.com>
2019-11-26 01:49:08 -0500

    Remove trading context design

--- api/api.py
+++ api/api.py
@@ -2,19 +2,14 @@ class API():
   def __init__(self, api_config):
     self.name = ""
     self.base_url = ""
-    self.api_key = ""
     self.set_api_config(api_config)
 
   def set_api_config(self, api_config):
     try:
       self.name = api_config['name']
       self.base_url = api_config['base_url']
-      self.api_key = api_config['api_key']
     except:
       print("Error in setting up API: " + self.name)
-
-  def set_trading_context(self, context):
-    self.context = context
 
   def set_positions(self, system_id, symbol, symbol_type, position):
     print("set_position: {} {} {}".format(system_id, symbol, position))
